fb6507673188a66c8856d248f0e39326332bd820
Boluwatife <boluwatifeoladejo@gmail>
2023-01-06 13:29:03 +0000

    Fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,7 +71,7 @@ class DBStorage:
             self.session.delete(obj)
 
     def reload(self):
-        """configuration base
+        """configuration
         """
         Base.metadata.create_all(self.__engine)
         sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
